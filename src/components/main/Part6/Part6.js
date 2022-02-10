390423faa35f635dbf70f831e883b6ea9f77de5e
Staryxaizergil <42916293+Staryxaizergil@users.noreply.github.com>
2022-02-11 00:43:37 +0300

    Part6 without adaptive

--- src/components/main/Part6/Part6.js
+++ src/components/main/Part6/Part6.js
@@ -6,7 +6,73 @@ import img03 from './img/03.jpg';
 function Part6() {
 	return (
 		<div className="Part6">
-
+			<div className="container">
+				<div className="part6__top">
+					<div className="part6-top__title">LATEST FROM BLOG</div>
+					<div className="part6-top__link">
+						<a href="" className="part6-top-link__view">see all</a>
+					</div>
+				</div>
+				<div className="part6__columns">
+					<div className="part6__column">
+						<div className="part6-column__content">
+							<div className="part6-column-content__image">
+								<img src={img01} alt="" />
+							</div>
+							<div className="part6-column-content__info">
+								<div className="part6-info__title">The Easiest Way to Break</div>
+								<div className="part6-info__text">
+									<div className="part6-info-text__main">But I must explain to you how all this mistaken idea of denouncing pleas and praising pain was bor</div>
+									<div className="part6-info-text__date">
+										<div className="part6-info-date__date">April 6, 2032</div>
+										<div className="part-info-date__readmore">
+											<a href="" className="part6-info-readmore__link">Read more</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="part6__column">
+						<div className="part6-column__content">
+							<div className="part6-column-content__image">
+								<img src={img02} alt="" />
+							</div>
+							<div className="part6-column-content__info">
+								<div className="part6-info__title">The Easiest Way to Break</div>
+								<div className="part6-info__text">
+									<div className="part6-info-text__main">But I must explain to you how all this mistaken idea of denouncing pleas and praising pain was bor</div>
+									<div className="part6-info-text__date">
+										<div className="part6-info-date__date">April 6, 2032</div>
+										<div className="part-info-date__readmore">
+											<a href="" className="part6-info-readmore__link">Read more</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="part6__column">
+						<div className="part6-column__content">
+							<div className="part6-column-content__image">
+								<img src={img03} alt="" />
+							</div>
+							<div className="part6-column-content__info">
+								<div className="part6-info__title">The Easiest Way to Break</div>
+								<div className="part6-info__text">
+									<div className="part6-info-text__main">But I must explain to you how all this mistaken idea of denouncing pleas and praising pain was bor</div>
+									<div className="part6-info-text__date">
+										<div className="part6-info-date__date">April 6, 2032</div>
+										<div className="part-info-date__readmore">
+											<a href="" className="part6-info-readmore__link">Read more</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
