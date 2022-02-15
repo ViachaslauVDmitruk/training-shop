cc6f8177ebbd8f19dcb8f957e6ad2e7cef114573
Staryxaizergil <42916293+Staryxaizergil@users.noreply.github.com>
2022-02-15 21:16:17 +0300

    href chenge "/"

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import { Link, HashRouter as Router, Route } from 'react-router-dom';
+// import { Link, HashRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
-import CategoriesMan from './pages/Categories/CategoriesMan';
-import CategoriesWoman from './pages/Categories/CategoriesWoman';
+// import CategoriesMan from './pages/Categories/CategoriesMan';
+// import CategoriesWoman from './pages/Categories/CategoriesWoman';
 // import ProductPage from './pages/ProductPage/ProductPage';
 
 function App() {
 	return (
 		<div className='app' data-test-id='app'>
-			<Router>
+			{/* <Router>
 				<Link to="/">Home</Link>
 				<Link to="/woman">woman</Link>
 				<Route parth="/" component={Home}></Route>
 				<Route parth="/woman" component={CategoriesWoman}></Route>
 				<Route parth="/man" component={CategoriesMan}></Route>
-			</Router>
+			</Router> */}
+			<Home />
 		</div >
 	);
 }
