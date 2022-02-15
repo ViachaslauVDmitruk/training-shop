1ff03080799741beaff35dfc49cf95fa501a9f31
Staryxaizergil <42916293+Staryxaizergil@users.noreply.github.com>
2022-02-15 20:30:13 +0300

    Check mistakes 2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 // import CategoriesWoman from './pages/Categories/CategoriesWoman';
 // import CategoriesMan from './pages/Categories/CategoriesMan';
