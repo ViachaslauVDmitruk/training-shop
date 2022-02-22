de006a0889e35795752c4ba481226e9ea26621ab
Staryxaizergil <42916293+Staryxaizergil@users.noreply.github.com>
2022-02-22 19:30:21 +0300

    Created SliderMain in Part1 with adaptive

--- src/components/main/Part1/Part1.js
+++ src/components/main/Part1/Part1.js
@@ -1,11 +1,12 @@
 import { Link } from 'react-router-dom';
+import SliderMain from '../../slidermain/SliderMain';
 import './css/Part1.css';
-import img01 from './img/01.jpg';
+
 import img02 from './img/02.jpg';
 import img03 from './img/03.jpg';
 import img04 from './img/04.jpg';
-// import arrL from './img/ArrowL.png';
-// import arrR from './img/ArrowR.png';
+import arrL from './img/ArrowL.png';
+import arrR from './img/ArrowR.png';
 
 function Part1() {
 	return (
@@ -13,20 +14,12 @@ function Part1() {
 			<section className="content">
 				<div className="content__items">
 					<div className="content-items__item content-items__item_big">
-						<div className="content-items-item__image">
-							<img src={img01} alt="img01" />
-							<div className="content-slider">
-								<div className="content-items-item__slider">
-									{/* <div className="content-block__arrow">
-											<img src={arrL} alt="arrL" />
-										</div> */}
-									<div className="content-items-item-slider__content">
-										<div className="content-items-item-link__title">BUNNER</div>
-										<div className="content-items-item-link__text">YOUR TITLE TEXT</div>
-									</div>
-									{/* <div className="content-block__arrow">
-										<img src={arrR} alt="arrR" />
-									</div> */}
+						<SliderMain />
+						<div className="content-slider">
+							<div className="content-items-item__slider">
+								<div className="content-items-item-slider__content">
+									<div className="content-items-item-link__title">BUNNER</div>
+									<div className="content-items-item-link__text">YOUR TITLE TEXT</div>
 								</div>
 							</div>
 						</div>
@@ -60,8 +53,8 @@ function Part1() {
 						</div>
 					</div>
 				</div>
-			</section>
-		</div>
+			</section >
+		</div >
 	);
 }
 
---
+++ src/components/slidermain/SliderMain.js
@@ -0,0 +1,29 @@
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "../../App.css";
+// import required modules
+import { Navigation } from "swiper";
+import img01 from '../main/Part1/img/01.jpg';
+
+function SliderMain() {
+	return (
+		<Swiper navigation={true} modules={[Navigation]} className="content-items-item__image"
+		>
+			<SwiperSlide>
+				<img src={img01} alt="img01" />
+			</SwiperSlide>
+			<SwiperSlide>
+				<img src={img01} alt="img01" />
+			</SwiperSlide>
+			<SwiperSlide>
+				<img src={img01} alt="img01" />
+			</SwiperSlide>
+		</Swiper>
+	);
+}
+export default SliderMain;
+
