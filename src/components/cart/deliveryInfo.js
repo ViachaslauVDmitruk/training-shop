e72603bed4f88143279777fb7a8360ae0bcec2c2
ViachaslauVDmitruk <42916293+ViachaslauVDmitruk@users.noreply.github.com>
2022-04-11 12:52:52 +0300

    Created Delivery and Pay components

--- src/components/cart/deliveryInfo.js
+++ src/components/cart/deliveryInfo.js
@@ -1,227 +1,237 @@
 import { Field, ErrorMessage } from 'formik';
 import reactInputMask from 'react-input-mask';
-import { useDispatch, useSelector } from 'react-redux';
-import { requestDataCountries } from '../../redux/Shopping/shopping-actions';
 import './css/deliveryInfo.css';
 import TextErrorDelivery from './errorDelivery';
-import StorePickupData from './storуAdressInfo';
+import StorePickupData from './storeAdressInfo';
+import { useEffect } from 'react';
+import TotalPrice from './totalPrise';
+import DeliveryPayButton from './devileryPayButton';
 
 function DeliveryInfo(props) {
-	const countries = useSelector(store => store.shop.countries);
-	const dispatch = useDispatch();
-	// console.log('props', props.formik)
+  useEffect(() => {
+    if (props.formik.values.deliveryMethod === 'Pickup from post offices') {
+      props.formik.setValues({ ...props.formik.values, country: 'Беларусь' });
+    } else {
+      props.formik.setValues({ ...props.formik.values, country: '' });
+    }
+  }, [props.formik.values.deliveyMethod, props.formik]);
 
-	return (
-		<div className="delivery-info">
-			<div className="choose-method">
-				<div className="choose-method__title">Choose the method of delivery of the items</div>
-				<div className='choose-method__radio'>
-					<label className='radio-button'>
-						<Field
-							type="radio"
-							id="Choise_1"
-							name="method"
-							className="choose-method__item"
-							value="Pickup from post offices"
-						/>
-						<span className='radio-lable'>Pickup from post offices</span></label>
-				</div>
-				<div className='choose-method__radio'>
-					<label className='radio-button'>
-						<Field
-							type="radio"
-							id="Choise_2"
-							name="method"
-							className="choose-method__item"
-							value="Express delivery"
-
-						/>
-						<span className='radio-lable'>Express delivery</span></label>
-				</div>
-				<div className='choose-method__radio'>
-					<label className='radio-button'>
-						<Field
-							type="radio"
-							id="Choise_3"
-							name="method"
-							className="choose-method__item"
-							value="Store pickup"
-
-						/>
-						<span className='radio-lable'>Store pickup</span></label>
-				</div >
-			</div>
-			<div className="choose-info">
-				<div htmlFor="phone" className='choose-info__title'>Phone</div>
-				<div className='choose-info__item'>
-					<Field
-						autoComplete="off"
-						as={reactInputMask}
-						type="tel"
-						id="phone"
-						name="phone"
-						className="choose-info__input"
-						placeholder="+375  (_ _) _ _ _ _ _ _ _"
-						mask={"+375 (99) 9999999"}
-						style={(props.formik.touched.phone && !props.formik.values.phone) ? { border: "1px solid red" } : null}
-					/>
-					<ErrorMessage name="phone" component={TextErrorDelivery} />
-				</div>
-				<label htmlFor="mail" className='choose-info__title'>E-mail</label>
-				<div className='choose-info__item'>
-					<Field
-						autoComplete="off"
-						type="text"
-						id="mail"
-						name="mail"
-						className="choose-info__input"
-						placeholder="e-mail"
-						style={(props.formik.touched.mail && !props.formik.values.mail) ? { border: "1px solid red" } : null}
-					/>
-					<ErrorMessage name="mail" component={TextErrorDelivery} />
-				</div>
-
-				{(props.formik.values.method !== "Store pickup") && <div>
-					<div htmlFor="adress" className='choose-info__title'>Adress</div>
-					{(props.formik.values.method === "Pickup from post offices") && <div className='choose-info__item'>
-						<Field
-							autoComplete="off"
-							type="text"
-							id="adress"
-							name="country"
-							className="choose-info__input"
-							placeholder="Country"
-							value={(props.formik.values.method === "Pickup from post offices") ? "Беларусь" : ''}
-						/>
-						<ErrorMessage name="country" component={TextErrorDelivery} />
-					</div>}
-					{(props.formik.values.method === "Express delivery") && <div className='choose-info__item'>
-						<Field
-							as="select"
-							autoComplete="off"
-							type="text"
-							id="adress"
-							name="country"
-							className="choose-info__input"
-							placeholder="Country"
-
-							style={(props.formik.touched.country && !props.formik.values.country) ? { border: "1px solid red" } : null}
-						>
-
-						</Field>
-						<ErrorMessage name="country" component={TextErrorDelivery} />
-					</div>}
-					<div className='choose-info__item'>
-						<Field
-							autoComplete="off"
-							type="text"
-							name="city"
-							className="choose-info__input"
-							placeholder="City"
-							style={(props.formik.touched.city && !props.formik.values.city) ? { border: "1px solid red" } : null}
-						/>
-						<ErrorMessage name="city" component={TextErrorDelivery} />
-					</div>
-					<div className='choose-info__item'>
-						<Field
-							autoComplete="off"
-							type="text"
-							name="street"
-							className="choose-info__input"
-							placeholder="Street"
-							style={(props.formik.touched.street && !props.formik.values.street) ? { border: "1px solid red" } : null}
-						/>
-						<ErrorMessage name="street" component={TextErrorDelivery} />
-					</div>
-					<div className='choose-info__item house'>
-						<div className='choose-info-house'>
-							<Field
-								autoComplete="off"
-								type="text"
-								name="house"
-								className="choose-info__input"
-								placeholder="House"
-								style={(props.formik.touched.house && !props.formik.values.house) ? { border: "1px solid red" } : null}
-							/>
-							<ErrorMessage name="house" component={TextErrorDelivery} />
-						</div>
-						<div className='choose-info-house'>
-							<Field
-								autoComplete="off"
-								type="text"
-								name="apartment"
-								className="choose-info__input"
-								placeholder="Apartment"
-							/>
-						</div>
-					</div>
-				</div>}
-			</div>
-			{
-				(props.formik.values.method === "Pickup from post offices") && <div className="choose-postcode">
-					<label htmlFor="post-code" className='choose-info__title'>Post code</label>
-					<div className='choose-info__item'>
-						<Field
-							autoComplete="off"
-							as={reactInputMask}
-							type="text"
-							id="post-code"
-							name="postcode"
-							className="choose-info__input"
-							placeholder="BY _ _ _ _ _ _"
-							mask={"BY 999999"}
-							style={(props.formik.touched.postcode && !props.formik.values.postcode) ? { border: "1px solid red" } : null}
-						/>
-						<ErrorMessage name="postcode" component={TextErrorDelivery} />
-					</div>
-				</div>
-			}
-			{(props.formik.values.method === "Store pickup") && <div>
-				{/* <div htmlFor="storeAdress" className='choose-info__title'>Adress of store</div>
-				<div className='choose-info__item'>
-					<Field
-						type="text"
-						id="storeAdress"
-						name="country"
-						className="choose-info__input"
-						placeholder="Counrty"
-						style={props.formik.touched.country ? { border: "1px solid red" } : null}
-					/>
-					<ErrorMessage name="storeAdress" component={TextErrorDelivery} />
-				</div>
-				<div className='choose-info__item'>
-					<Field
-						type="text"
-						id="storeAdress"
-						name="storeAdress"
-						className="choose-info__input"
-						placeholder="Store adress"
-						style={props.formik.touched.storeAdress ? { border: "1px solid red" } : null}
-					/>
-					<ErrorMessage name="storeAdress" component={TextErrorDelivery} />
-				</div> */}
-				<StorePickupData />
-			</div>}
-			<div >
-				<div className='agree-information__block'>
-					<label className="agree-information">
-						<Field
-							type="checkbox"
-							id="check"
-							name="check"
-							className="check-agree"
-						/>
-						<span className='check-box' style={!props.formik.values.check ? { border: "1px solid red" } : null}></span>
-						<span className="check-agree-text">I agree to the processing of my personal information</span>
-					</label>
-					<ErrorMessage name="check" component={TextErrorDelivery} />
-				</div>
-			</div>
-		</div>
-	)
-	// 		}}
-	// 	</Formik >
-	// )
+  return (
+    <div className="delivery-info">
+      <div className="wrapper-info">
+        <div className="choose-method">
+          <div className="choose-method__title">Choose the method of delivery of the items</div>
+          <div className="choose-method__radio">
+            <label className="radio-button">
+              <Field
+                type="radio"
+                id="Choise_1"
+                name="deliveryMethod"
+                className="choose-method__item"
+                value="Pickup from post offices"
+              />
+              <span className="radio-lable">Pickup from post offices</span>
+            </label>
+          </div>
+          <div className="choose-method__radio">
+            <label className="radio-button">
+              <Field
+                type="radio"
+                id="Choise_2"
+                name="deliveryMethod"
+                className="choose-method__item"
+                value="Express delivery"
+              />
+              <span className="radio-lable">Express delivery</span>
+            </label>
+          </div>
+          <div className="choose-method__radio">
+            <label className="radio-button">
+              <Field
+                type="radio"
+                id="Choise_3"
+                name="deliveryMethod"
+                className="choose-method__item"
+                value="Store pickup"
+              />
+              <span className="radio-lable">Store pickup</span>
+            </label>
+          </div>
+        </div>
+        <div className="choose-info">
+          <div htmlFor="phone" className="choose-info__title">
+            Phone
+          </div>
+          <div className="choose-info__item">
+            <Field
+              autoComplete="off"
+              as={reactInputMask}
+              type="tel"
+              id="phone"
+              name="phone"
+              className="choose-info__input"
+              placeholder="+375  (_ _) _ _ _ _ _ _ _"
+              mask={'+375 (99) 9999999'}
+              style={props.formik.touched.phone && !props.formik.values.phone ? { border: '1px solid red' } : null}
+            />
+            <ErrorMessage name="phone" component={TextErrorDelivery} />
+          </div>
+          <label htmlFor="mail" className="choose-info__title">
+            E-mail
+          </label>
+          <div className="choose-info__item">
+            <Field
+              autoComplete="off"
+              type="text"
+              id="mail"
+              name="mail"
+              className="choose-info__input"
+              placeholder="e-mail"
+              style={props.formik.touched.mail && !props.formik.values.mail ? { border: '1px solid red' } : null}
+            />
+            <ErrorMessage name="mail" component={TextErrorDelivery} />
+          </div>
+          {props.formik.values.method !== 'Store pickup' && (
+            <div>
+              <div htmlFor="adress" className="choose-info__title">
+                Adress
+              </div>
+              {
+                // props.formik.values.method === 'Pickup from post offices'
+                //  &&
+                <div className="choose-info__item">
+                  <Field
+                    autoComplete="off"
+                    type="text"
+                    id="adress"
+                    name="country"
+                    className="choose-info__input"
+                    placeholder="Country"
+                    value={
+                      props.formik.values.country
+                      // props.formik.values.method === 'Pickup from post offices' ? 'Беларусь' : '
+                      // '
+                    }
+                  />
+                  <ErrorMessage name="country" component={TextErrorDelivery} />
+                </div>
+              }
+              {props.formik.values.method === 'Express delivery' && (
+                <div className="choose-info__item">
+                  <Field
+                    autoComplete="off"
+                    type="text"
+                    id="adress"
+                    name="country"
+                    className="choose-info__input"
+                    placeholder="Country"
+                    style={
+                      props.formik.touched.country && !props.formik.values.country ? { border: '1px solid red' } : null
+                    }
+                  ></Field>
+                  <ErrorMessage name="country" component={TextErrorDelivery} />
+                </div>
+              )}
+              <div className="choose-info__item">
+                <Field
+                  autoComplete="off"
+                  type="text"
+                  name="city"
+                  className="choose-info__input"
+                  placeholder="City"
+                  style={props.formik.touched.city && !props.formik.values.city ? { border: '1px solid red' } : null}
+                />
+                <ErrorMessage name="city" component={TextErrorDelivery} />
+              </div>
+              <div className="choose-info__item">
+                <Field
+                  autoComplete="off"
+                  type="text"
+                  name="street"
+                  className="choose-info__input"
+                  placeholder="Street"
+                  style={
+                    props.formik.touched.street && !props.formik.values.street ? { border: '1px solid red' } : null
+                  }
+                />
+                <ErrorMessage name="street" component={TextErrorDelivery} />
+              </div>
+              <div className="choose-info__item house">
+                <div className="choose-info-house">
+                  <Field
+                    autoComplete="off"
+                    type="text"
+                    name="house"
+                    className="choose-info__input"
+                    placeholder="House"
+                    style={
+                      props.formik.touched.house && !props.formik.values.house ? { border: '1px solid red' } : null
+                    }
+                  />
+                  <ErrorMessage name="house" component={TextErrorDelivery} />
+                </div>
+                <div className="choose-info-house">
+                  <Field
+                    autoComplete="off"
+                    type="text"
+                    name="apartment"
+                    className="choose-info__input"
+                    placeholder="Apartment"
+                  />
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+        {props.formik.values.method === 'Pickup from post offices' && (
+          <div className="choose-postcode">
+            <label htmlFor="post-code" className="choose-info__title">
+              Post code
+            </label>
+            <div className="choose-info__item">
+              <Field
+                autoComplete="off"
+                as={reactInputMask}
+                type="text"
+                id="post-code"
+                name="postcode"
+                className="choose-info__input"
+                placeholder="BY _ _ _ _ _ _"
+                mask={'BY 999999'}
+                style={
+                  props.formik.touched.postcode && !props.formik.values.postcode ? { border: '1px solid red' } : null
+                }
+              />
+              <ErrorMessage name="postcode" component={TextErrorDelivery} />
+            </div>
+          </div>
+        )}
+        {props.formik.values.method === 'Store pickup' && (
+          <div>
+            <StorePickupData formik={props.formik} />
+          </div>
+        )}
+        <div>
+          <div className="agree-information__block">
+            <label className="agree-information">
+              <Field type="checkbox" id="check" name="check" className="check-agree" />
+              <span
+                className="check-box"
+                style={!props.formik.values.check ? { border: '1px solid red' } : null}
+              ></span>
+              <span className="check-agree-text">I agree to the processing of my personal information</span>
+            </label>
+            <ErrorMessage name="check" component={TextErrorDelivery} />
+          </div>
+        </div>
+      </div>
+      <TotalPrice totalPrice={props.totalPrice} />
+      <DeliveryPayButton type={'onSubmit'} title={props.title} step={3} setStep={props.setStep} />
+    </div>
+  );
+  // 		}}
+  // 	</Formik >
+  // )
 }
 
 export default DeliveryInfo;
--- src/components/cart/payment.js
+++ src/components/cart/payment.js
@@ -1,135 +1,132 @@
 import { Field, ErrorMessage } from 'formik';
+import { useEffect } from 'react';
 import reactInputMask from 'react-input-mask';
+import DeliveryPayButton from './devileryPayButton';
 
 import TextErrorDelivery from './errorDelivery';
 import imgLogo01 from './img/01.png';
 import imgLogo02 from './img/02.png';
 import imgLogo03 from './img/03.png';
-
+import TotalPrice from './totalPrise';
 
 function Payment(props) {
-	console.log('leangth', props.formik.values.cardCVV.length)
-	return (
-		<div className="delivery-info">
-			<div className="choose-method">
-				<div className="choose-method__title">Method of payments</div>
-				<div className='choose-method__radio'>
-					<label htmlFor='choise_1' className='radio-button'>
-						<Field
-							type="radio"
-							id="choise_1"
-							name="paymentMethod"
-							className="choose-method__item"
-							value="PayPal"
-						/>
-						<span className='radio-lable'></span>
-						<img src={imgLogo01} alt="imgLogo" />
-					</label>
-				</div>
-				<div className='choose-method__radio'>
-					<label htmlFor='choise_2' className='radio-button'>
-						<Field
-							type="radio"
-							id="choise_2"
-							name="paymentMethod"
-							className="choose-method__item"
-							value="Card"
-							checked
-						/>
-						<span className='radio-lable'></span>
-						<img src={imgLogo02} alt="imgLogo" />
-					</label>
-				</div>
-				<div className='choose-method__radio'>
-					<label htmlFor='choise_3' className='radio-button'>
-						<Field
-							type="radio"
-							id="choise_3"
-							name="paymentMethod"
-							className="choose-method__item"
-							value="Card" />
-						<span className='radio-lable'></span>
-						<img src={imgLogo03} alt="imgLogo" />
-					</label>
-				</div >
-				<div className='choose-method__radio'>
-					<label htmlFor='choise_4' className='radio-button'>
-						<Field
-							type="radio"
-							id="choise_4"
-							name="paymentMethod"
-							className="choose-method__item"
-							value="Cash" />
-						<span className='radio-lable'>Cash</span>
-					</label>
-				</div >
-			</div>
-			<div className="choose-info">
-				{(props.formik.values.paymentMethod === "PayPal") &&
-					<div>
-						<div className='choose-info__title'>E-mail</div>
-						<div className='choose-info__item'>
+  useEffect(() => {
+    props.formik.values.paymentMethod = 'Card';
+  }, [props.formik.values]);
 
-							<Field
-								type="text"
-								id="mail"
-								name="cashEmail"
-								className="choose-info__input"
-								placeholder="e-mail"
-								style={props.formik.touched.cashEmail ? { border: "1px solid red" } : null}
-							/>
-							<ErrorMessage name="cashEmail" component={TextErrorDelivery} />
-						</div>
-					</div>}
-				{(props.formik.values.paymentMethod === "Card") && <div>
-					<div className='choose-info__title'>Card</div>
-					<div className='choose-info__item'>
-						<Field
-							as={reactInputMask}
-							type="text"
-							id="adress"
-							name="card"
-							className="choose-info__input"
-							placeholder="---- ---- ---- ----"
-							mask={"9999 9999 9999 9999"}
-							style={props.formik.touched.card && !props.formik.values.card ? { border: "1px solid red" } : null}
-						/>
-						<ErrorMessage name="card" component={TextErrorDelivery} />
-					</div>
-					<div className='choose-info__item house'>
-						<div className='choose-info-house'>
-							<Field
-								// as={reactInputMask}
-								type="text"
-								name="cardDate"
-								className="choose-info__input"
-								placeholder="MM/YY"
-								// mask={"99/99"}
-								style={props.formik.touched.cardDate && !props.formik.values.cardData ? { border: "1px solid red" } : { border: "none" }}
-							/>
-							<ErrorMessage name="cardDate" component={TextErrorDelivery} />
-						</div>
-						<div className='choose-info-house'>
-							<Field
-								// as={reactInputMask}
-								type="text"
-								name="cardCVV"
-								className="choose-info__input"
-								placeholder="CVV"
-								// mask={"999"}
-								style={props.formik.touched.cardCVV && !props.formik.values.cardCVV ? { border: "1px solid red" } : null}
-							/>
-							<ErrorMessage name="cardCVV" component={TextErrorDelivery} />
-						</div>
-					</div>
-				</div>}
-			</div>
-		</div>
-	)
-	// 		}}
-	// 	</Formik >
+  return (
+    <div className="delivery-info">
+      <div className="wrapper-info">
+        <div className="choose-method">
+          <div className="choose-method__title">Method of payments</div>
+          <div className="choose-method__radio">
+            <label htmlFor="choise_1" className="radio-button">
+              <Field type="radio" id="choise_1" name="paymentMethod" className="choose-method__item" value="PayPal" />
+              <span className="radio-lable"></span>
+              <img src={imgLogo01} alt="imgLogo" />
+            </label>
+          </div>
+          <div className="choose-method__radio">
+            <label htmlFor="choise_2" className="radio-button">
+              <Field type="radio" id="choise_2" name="paymentMethod" className="choose-method__item" value="Card" />
+              <span className="radio-lable"></span>
+              <img src={imgLogo02} alt="imgLogo" />
+            </label>
+          </div>
+          <div className="choose-method__radio">
+            <label htmlFor="choise_3" className="radio-button">
+              <Field type="radio" id="choise_3" name="paymentMethod" className="choose-method__item" value="Card" />
+              <span className="radio-lable"></span>
+              <img src={imgLogo03} alt="imgLogo" />
+            </label>
+          </div>
+          <div className="choose-method__radio">
+            <label htmlFor="choise_4" className="radio-button">
+              <Field type="radio" id="choise_4" name="paymentMethod" className="choose-method__item" value="Cash" />
+              <span className="radio-lable">Cash</span>
+            </label>
+          </div>
+        </div>
+        <div className="choose-info">
+          {props.formik.values.paymentMethod === 'PayPal' && (
+            <div>
+              <div className="choose-info__title">E-mail</div>
+              <div className="choose-info__item">
+                <Field
+                  type="text"
+                  id="mail"
+                  name="cashEmail"
+                  className="choose-info__input"
+                  placeholder="e-mail"
+                  style={
+                    props.formik.touched.cashEmail && props.formik.values.cashEmail ? { border: '1px solid red' } : null
+                  }
+                />
+                <ErrorMessage name="cashEmail" component={TextErrorDelivery} />
+              </div>
+            </div>
+          )}
+          {props.formik.values.paymentMethod === 'Card' && (
+            <div>
+              <div className="choose-info__title">Card</div>
+              <div className="choose-info__item">
+                <Field
+                  as={reactInputMask}
+                  type="text"
+                  id="adress"
+                  name="card"
+                  className="choose-info__input"
+                  placeholder="---- ---- ---- ----"
+                  mask={'9999 9999 9999 9999'}
+                  style={props.formik.touched.card && !props.formik.values.card ? { border: '1px solid red' } : null}
+                />
+                <ErrorMessage name="card" component={TextErrorDelivery} />
+              </div>
+              <div className="choose-info__item house">
+                <div className="choose-info-house">
+                  <Field
+                    as={reactInputMask}
+                    type="text"
+                    name="cardDate"
+                    className="choose-info__input"
+                    placeholder="MM/YY"
+                    mask={'99/99'}
+                    style={
+                      props.formik.touched.cardDate && !props.formik.values.cardData
+                        ? { border: '1px solid red' }
+                        : { border: 'none' }
+                    }
+                  />
+                  <ErrorMessage name="cardDate" component={TextErrorDelivery} />
+                </div>
+                <div className="choose-info-house">
+                  <Field
+                    // as={reactInputMask}
+                    type="text"
+                    name="cardCVV"
+                    className="choose-info__input"
+                    placeholder="CVV"
+                    // mask={"999"}
+                    style={
+                      props.formik.touched.cardCVV && !props.formik.values.cardCVV ? { border: '1px solid red' } : null
+                    }
+                  />
+                  <ErrorMessage name="cardCVV" component={TextErrorDelivery} />
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+      <TotalPrice totalPrice={props.totalPrice} />
 
-	// )
+      <DeliveryPayButton title={props.formik.values.paymentMethod !== 'Cash' ? props.titleOne : props.titleTwo} />
+    </div>
+  );
+  // 		}}
+  // 	</Formik >
+
+  // )
 }
 
 export default Payment;
