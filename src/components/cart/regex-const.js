1421e7e16b788af9a0e06a737566d6a55d66c7a1
Staryxaizergil <42916293+Staryxaizergil@users.noreply.github.com>
2022-04-19 00:53:38 +0300

    Created file regex-const

---
+++ src/components/cart/regex-const.js
@@ -0,0 +1,5 @@
+export const regExMail = /^[_a-z0-9-\\+-][^\s]+(\.[_a-z0-9-]\+)*@[a-z0-9-]+(\.[a-z0-9-]\+)*(\.[a-z]{2,4})$/i;
+export const regExPhone = /^(\+375|80)\s\((29|25|44|33)\)\s[0-9]{3}[0-9]{2}[0-9]{2}$/;
+export const regExCard = /^[\d\s]+$/;
+export const regExCardDate = /^(0\d|1[0-2])\/(([2-9][2-9])|[3-9]\d)$/;
+export const regExCVV = /^\d+$/;
